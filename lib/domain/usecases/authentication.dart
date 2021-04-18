c1477d53b0b78b814de3b8d9e5efff63415fb0d7
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2021-04-18 11:40:02 -0300

    chenge Authentication to recive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,10 +2,22 @@ import 'package:curso_tdd/domain/entities/account_entity.dart';
 
 import 'package:meta/meta.dart';
 
-abstract class Authentication{
-
+abstract class Authentication {
   //Vai retornar um objeto o tipo AccountEntity
-  Future<AccountEntity>auth({@required String email, @required String password});
+  Future<AccountEntity> auth(AuthenticationParans parans);
+  }
+  
+  class AuthenticationParans {
+    final String email;
+    final String secret;
+
+  AuthenticationParans({
+    @required this.email, 
+    @required this.secret
+  });
+
+ 
+
 
 
 }
